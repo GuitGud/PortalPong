cc828abf5e125c8a5942de9c67f81705c53eba56
Guilherme Alcoforado Rabelo <113638656+GuitGud@users.noreply.github.com>
2023-11-21 08:44:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
 #define BALL_SPEED    50
 #define PADDLE_SPEED  10
 #define SCORE_LIMIT   20
+#define PADDLE_INCREMENT (PADDLE_HEIGHT*2 / 5)
+#define PORTAL_HEIGHT 4
+#define PORTAL_SPEED  1
 
 typedef struct {
     int x, y;
@@ -20,7 +23,21 @@ typedef struct {
     int x, y;
 } Paddle;
 
-void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, int score1, int score2) {
+typedef struct {
+    int x, y;
+    int dx;
+} Portal;
+
+void movePortal(Portal *portal) {
+    portal->x += portal->dx;
+
+    // Verificar se o portal atingiu as bordas e inverter a direção se necessário
+    if (portal->x <= SCRSTARTX || portal->x >= SCRENDX - 2) {
+        portal->dx = -portal->dx;
+    }
+}
+//desenha a tela do jogo com os elementos dentro
+void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, Portal portalTop, Portal portalBottom, int score1, int score2) {
     screenClear();
 
     // Draw top border
@@ -41,6 +58,14 @@ void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, int score1, int score2
     screenGotoxy(ball.x, ball.y);
     printf("o");
 
+    // Desenhar portais
+    for (int i = 0; i < PORTAL_HEIGHT; i++) {
+        screenGotoxy(portalTop.x, portalTop.y +2 + i);
+        printf("][");
+        screenGotoxy(portalBottom.x, portalBottom.y + i);
+        printf("][");
+    }
+
     // Draw bottom border
     screenGotoxy(0, SCRENDY);
     for (int i = 0; i < SCRENDX; i++) {
@@ -56,13 +81,19 @@ void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, int score1, int score2
 
 int main() {
     // Initialize screen, keyboard, and timer
-    screenInit(0);  // Draw borders in drawFrame
+    screenInit(0);
     keyboardInit();
     timerInit(BALL_SPEED);
 
     // Initialize game variables
     Paddle paddle1 = {SCRSTARTX, SCRENDY / 2 - PADDLE_HEIGHT / 2};
     Paddle paddle2 = {SCRENDX - PADDLE_WIDTH, SCRENDY / 2 - PADDLE_HEIGHT / 2};
+
+    // Altere as coordenadas iniciais dos portais para o centro da tela
+    Portal portalTop = {SCRENDX / 2, SCRENDY / 4, PORTAL_SPEED};
+    Portal portalBottom = {SCRENDX / 2, 3 * SCRENDY / 4 - PORTAL_HEIGHT, PORTAL_SPEED};
+
+    // Ajuste a posição inicial da bola
     Ball ball = {SCRENDX / 2, SCRENDY / 2, -1, 1};
     int score1 = 0, score2 = 0;
 
@@ -71,14 +102,14 @@ int main() {
         // Check for input
         if (keyhit()) {
             char key = readch();
-            if (key == 'w' && paddle1.y > SCRSTARTY) {
-                paddle1.y -= PADDLE_SPEED;
+            if (key == 'w' && paddle1.y > SCRSTARTY + 1) {
+                paddle1.y -= PADDLE_INCREMENT;
             } else if (key == 's' && paddle1.y < SCRENDY - PADDLE_HEIGHT) {
-                paddle1.y += PADDLE_SPEED;
-            } else if (key == 'i' && paddle2.y > SCRSTARTY) {
-                paddle2.y -= PADDLE_SPEED;
+                paddle1.y += PADDLE_INCREMENT;
+            } else if (key == 'i' && paddle2.y > SCRSTARTY + 1) {
+                paddle2.y -= PADDLE_INCREMENT;
             } else if (key == 'k' && paddle2.y < SCRENDY - PADDLE_HEIGHT) {
-                paddle2.y += PADDLE_SPEED;
+                paddle2.y += PADDLE_INCREMENT;
             }
         }
 
@@ -108,21 +139,40 @@ int main() {
             ball.y = SCRENDY / 2;
         }
 
-        // Reset scores if limit is reached
-        if (score1 >= SCORE_LIMIT || score2 >= SCORE_LIMIT) {
-            score1 = 0;
-            score2 = 0;
+        // Check for portal entry
+        if (ball.x == portalTop.x && ball.y >= portalTop.y && ball.y < portalTop.y + PORTAL_HEIGHT) {
+            ball.x = portalBottom.x;
+            ball.y = portalBottom.y + PORTAL_HEIGHT;
+            ball.dy = -ball.dy;
+        } else if (ball.x == portalBottom.x && ball.y >= portalBottom.y && ball.y < portalBottom.y + PORTAL_HEIGHT) {
+            ball.x = portalTop.x;
+            ball.y = portalTop.y - PORTAL_HEIGHT;
+            ball.dy = -ball.dy;
         }
 
-        // Draw the frame
-        drawFrame(paddle1, paddle2, ball, score1, score2);
+        // Reset scores if limit is reached
+        if (score1 >= SCORE_LIMIT || score2 >= SCORE_LIMIT) {
+            if (score1 >= SCORE_LIMIT) {
+                screenGotoxy(SCRENDX / 2 - 10, SCRENDY / 2);
+                printf("Left side wins! Press any key to exit.");
+            } else {
+                screenGotoxy(SCRENDX / 2 - 10, SCRENDY / 2);
+                printf("Right side wins! Press any key to exit.");
+            }
 
-        // Wait for the next frame
+            screenUpdate();
+
+            while (!keyhit()) {}
+
+            break;
+        }
+
+        drawFrame(paddle1, paddle2, ball, portalTop, portalBottom, score1, score2);
+
         timerPrint();
         while (!timerTimeOver()) {}
     }
 
-    // Clean up and exit
     keyboardDestroy();
     timerDestroy();
     screenDestroy();

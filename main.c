d409e4d94aecefb03cccabe856ed9c7ace267557
Guilherme Alcoforado Rabelo <113638656+GuitGud@users.noreply.github.com>
2023-11-20 22:22:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include "keyboard.h"
 #include "timer.h"
 
-
 #define PADDLE_HEIGHT 5
 #define PADDLE_WIDTH  1
 #define BALL_SPEED    50
 #define PADDLE_SPEED  10
+#define SCORE_LIMIT   20
 
 typedef struct {
     int x, y;
@@ -20,31 +20,43 @@ typedef struct {
     int x, y;
 } Paddle;
 
-void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, int score) {
+void drawFrame(Paddle paddle1, Paddle paddle2, Ball ball, int score1, int score2) {
     screenClear();
+
+    // Draw top border
+    screenGotoxy(0, 0);
+    for (int i = 0; i < SCRENDX; i++) {
+        printf("-");
+    }
 
     // Draw paddles
     for (int i = 0; i < PADDLE_HEIGHT; i++) {
         screenGotoxy(paddle1.x, paddle1.y + i);
-        printf("%c", BOX_BLOCK);
+        printf("|");
         screenGotoxy(paddle2.x, paddle2.y + i);
-        printf("%c", BOX_BLOCK);
+        printf("|");
     }
 
     // Draw ball
     screenGotoxy(ball.x, ball.y);
-    printf("%c", BOX_DOT);
+    printf("o");
 
-    // Draw score
-    screenGotoxy(SCRSTARTX - 10, SCRSTARTY);
-    printf("Score: %d", score);
+    // Draw bottom border
+    screenGotoxy(0, SCRENDY);
+    for (int i = 0; i < SCRENDX; i++) {
+        printf("-");
+    }
+
+    // Draw scores
+    screenGotoxy(2, 1);
+    printf("Player 1: %2d | Player 2: %2d", score1, score2);
 
     screenUpdate();
 }
 
 int main() {
     // Initialize screen, keyboard, and timer
-    screenInit(1);
+    screenInit(0);  // Draw borders in drawFrame
     keyboardInit();
     timerInit(BALL_SPEED);
 
@@ -52,7 +64,7 @@ int main() {
     Paddle paddle1 = {SCRSTARTX, SCRENDY / 2 - PADDLE_HEIGHT / 2};
     Paddle paddle2 = {SCRENDX - PADDLE_WIDTH, SCRENDY / 2 - PADDLE_HEIGHT / 2};
     Ball ball = {SCRENDX / 2, SCRENDY / 2, -1, 1};
-    int score = 0;
+    int score1 = 0, score2 = 0;
 
     // Main game loop
     while (1) {
@@ -86,19 +98,24 @@ int main() {
         }
 
         // Check for scoring
-        if (ball.x < SCRSTARTX || ball.x > SCRENDX) {
-            score++;
+        if (ball.x < SCRSTARTX) {
+            score2++;
+            ball.x = SCRENDX / 2;
+            ball.y = SCRENDY / 2;
+        } else if (ball.x > SCRENDX) {
+            score1++;
             ball.x = SCRENDX / 2;
             ball.y = SCRENDY / 2;
         }
 
-        // Draw the frame
-        drawFrame(paddle1, paddle2, ball, score);
-
-        // Check for game over
-        if (score >= 20) {
-            break;
+        // Reset scores if limit is reached
+        if (score1 >= SCORE_LIMIT || score2 >= SCORE_LIMIT) {
+            score1 = 0;
+            score2 = 0;
         }
+
+        // Draw the frame
+        drawFrame(paddle1, paddle2, ball, score1, score2);
 
         // Wait for the next frame
         timerPrint();
